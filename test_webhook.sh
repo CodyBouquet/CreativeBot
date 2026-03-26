70e8dee6cf8dbd072e6f8e106e282ada38a7f130
Cody Bouquet <253360263+CodyBouquet@users.noreply.github.com>
2026-03-26 17:01:46 -0500

    Add Pipedrive webhook test to test script

--- test_webhook.sh
+++ test_webhook.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+echo "--- Arrivy webhook ---"
 curl -s -X POST http://localhost:5001/arrivy-webhook \
   -H "Content-Type: application/json" \
   -d '{"EVENT_TYPE":"TASK_UPDATED","OBJECT_EXTERNAL_ID":"29905","OBJECT_TEMPLATE_ID":5395407346073600}'
+echo ""
+
+echo "--- Pipedrive webhook (stage 10) ---"
+curl -s -X POST http://localhost:5001/pipedrive-webhook \
+  -H "Content-Type: application/json" \
+  -d '{"event":"updated.deal","current":{"id":29905,"stage_id":10,"status":"open"}}'
 echo ""
